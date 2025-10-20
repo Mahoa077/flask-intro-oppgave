afb0024206f090a9d2db796216edeb1c6dabdde5
Mahoa077 <mahoa077@osloskolen.no>
2025-10-20 11:26:07 +0200

    Før lunsj mandag, noe er feil

--- starter/app.py
+++ starter/app.py
@@ -3,16 +3,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('index.html')
-
+#lagde en liste over inlegg som jeg da kalte posts
 posts = [
-    [1, "Første innlegg", "<p>Innlegg 1"]
-    [2, "Andre innlegg", "<p>Innlegg 2"]
-    [3, "Tredje innlegg", "<p>Innlegg 3"]
+    [1, "Første innlegg", "<p>Innlegg 1"],
+    [2, "Andre innlegg", "<p>Innlegg 2"],
+    [3, "Tredje innlegg", "<p>Innlegg 3"],
     [4, "Fjerde innlegg", "<p>Innlegg 4"]
 ]
+
+@app.route("/")
+def hello():
+    return render_template('index.html', posts=posts)
+
+def get_post_by_id(post_id: int):
+    for p in posts:
+        if p[0] == post_id:
+            return p
+    return None
+
+@app.route("/post/<int:post_id>")
+def post_detail(post_id):
+    post = get_post_by_id(post_id)
+    if not post:
+        abort(404)
+    return render_template("post.html", post=post)
+
+from flask import abort
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404    
 
 if __name__ == "__main__":
     app.run(debug=True)
