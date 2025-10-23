ccff15ff33bb51df9a3e87de0e51791308e40c62
Mahoa077 <mahoa077@osloskolen.no>
2025-10-23 07:50:02 +0200

    torsdags morgen, ferdig med gårsdagens oppgaver

--- starter/app.py
+++ starter/app.py
@@ -1,52 +1,44 @@
+import sqlite3
 from flask import Flask, render_template, abort
  
 app = Flask(__name__)
- 
+DB_PATH = "blog.db" 
 # Hvert innlegg: [id, tittel, innhold_html]
 
-posts = [
-
-    [1, "Første innlegg", "<p>Hei og velkommen til mini-bloggen!</p>"],
-
-    [2, "Flask-tips", "<ul><li>Bruk <code>url_for</li><li>Arv fra base.html</li></ul>"],
-
-    [3, "Dagens sitat", "<blockquote>«Code is like humor. When you have to explain it, it’s bad.»</blockquote>"],
-
-]
+def fetch_all_posts():
+    with sqlite3.connect(DB_PATH) as con:
+        con.row_factory = sqlite3.Row
+        rows = con.execute(
+            "SELECT id, title, content from posts ORDER BY id DESC;"
+        ).fetchall()
+    return [[r["id"], r["title"], r["content"]] for r in rows]
  
-def get_post_by_id(post_id: int):
+def fetch_post_by_id(post_id: int):
+    with sqlite3.connect(DB_PATH) as con:
+        con.row_factory = sqlite3.Row
+        row = con.execute(
+            "SELECT id, title, content FROM posts WHERE id = ?;",
+            (post_id,)
+        ).fetchone()
+    if row is None:
+        return None
+    return [row["id"], row["title"], row["content"]]
 
-    for p in posts:
-
-        if p[0] == post_id:
-
-            return p
-
-    return None
- 
 @app.route("/")
-
 def index():
-
+    posts = fetch_all_posts()
     return render_template("index.html", posts=posts)
  
 @app.route("/post/<int:post_id>")
-
 def post_detail(post_id):
-
     post = get_post_by_id(post_id)
-
     if not post:
-
         abort(404)
-
     return render_template("post.html", post=post)
  
 
 @app.errorhandler(404)
-
 def page_not_found(e):
-
     return render_template("404.html"), 404
  
 if __name__ == "__main__":
